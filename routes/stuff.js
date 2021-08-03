68a18ff84950311e79043c4ebd04a3f53bca98ce
Kev_ <62744145+kevin-car@users.noreply.github.com>
2021-08-03 16:01:10 +0200

    bloc 3 OK, autorisations OK

--- routes/stuff.js
+++ routes/stuff.js
@@ -2,21 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 const stuffCtrl = require('../controllers/stuff')
+const auth = require('../middleware/auth')
 
 /* Création d'un nouveau produit */
-router.post('/', stuffCtrl.createThing);
+router.post('/', auth, stuffCtrl.createThing);
 
 /* Modification d'un objet existant avec l'ID en paramètre */
-router.put('/:id', stuffCtrl.modifyThing);
+router.put('/:id', auth, stuffCtrl.modifyThing);
 
 /* Supprimer un élément précis avec l'ID en paramètre */
-router.delete('/:id', stuffCtrl.deleteThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
 
 /* Afficher une page fiche produit avec l'ID en paramètre  */
-router.get('/:id', stuffCtrl.getOneThing);
+router.get('/:id', auth, stuffCtrl.getOneThing);
 
 /* Récupération de tous les objets (page d'accueil) */
-router.get('/', stuffCtrl.getAllThings);
+router.get('/', auth, stuffCtrl.getAllThings);
 
 /* Exporter le module router pour les routes */
 module.exports = router;
